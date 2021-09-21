776481c5ea1f78dd178986631d05aa0850fb5719
pechorka <pechor44@gmail.com>
2021-09-22 00:19:57 +0300

    added token description

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type Type string
+
+const (
+	ILLEGAL Type = "ILLEGAL"
+
+	// Identifiers + literals
+	IDENT Type = "IDENT" // add, foobar, x, y, ...
+	INT   Type = "INT"   // 1343456
+	// Operators
+	ASSIGN Type = "="
+	PLUS   Type = "+"
+	// Delimiters
+	COMMA     Type = ","
+	SEMICOLON Type = ";"
+	LPAREN    Type = "("
+	RPAREN    Type = ")"
+	LBRACE    Type = "{"
+	RBRACE    Type = "}"
+
+	// Keywords
+	FUNCTION Type = "FUNCTION"
+	LET      Type = "LET"
+)
+
+type Token struct {
+	Type    Type
+	Literal string
+}
