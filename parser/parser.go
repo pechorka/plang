c89baef640ee0eed8fcfa153a078b87f2f014aa8
pechorka <pechor44@gmail.com>
2021-10-02 00:52:55 +0300

    added parsing of let statements without value

--- parser/parser.go
+++ parser/parser.go
@@ -15,6 +15,7 @@ type Parser struct {
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
+	// fill cur and next token
 	p.readToken()
 	p.readToken()
 
@@ -27,5 +28,57 @@ func (p *Parser) readToken() {
 }
 
 func (p *Parser) Parse() *ast.Program {
+	var prog ast.Program
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			prog.Statements = append(prog.Statements, stmt)
+		}
+		p.readToken()
+	}
+	return &prog
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	}
+
 	return nil
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	stmt := ast.LetStatement{
+		Token: p.curToken,
+	}
+
+	if !p.isNextToken(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.isNextToken(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: add value parsing
+	for p.curToken.Type != token.SEMICOLON {
+		p.readToken()
+	}
+
+	return &stmt
+}
+
+func (p *Parser) isNextToken(tt token.Type) bool {
+	if p.nextToken.Type == tt {
+		p.readToken()
+		return true
+	}
+
+	return false
 }
