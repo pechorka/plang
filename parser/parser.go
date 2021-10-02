1f33fe92c0b3ca28d395cbb6d6c7e2e92b0a5394
pechorka <pechor44@gmail.com>
2021-10-02 18:34:15 +0300

    parser implementation

--- parser/parser.go
+++ parser/parser.go
@@ -8,15 +8,30 @@ import (
 	"github.com/pechorka/plang/token"
 )
 
+// precedences of operators;
+const (
+	_ int = iota
+	LOWEST
+)
+
+type (
+	prefixParseFn func() ast.Expression
+)
 type Parser struct {
-	l         *lexer.Lexer
-	curToken  token.Token
-	nextToken token.Token
-	errors    []string
+	l              *lexer.Lexer
+	curToken       token.Token
+	nextToken      token.Token
+	errors         []string
+	prefixParseFns map[token.Type]prefixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:              l,
+		prefixParseFns: make(map[token.Type]prefixParseFn),
+	}
+
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 
 	// fill cur and next token
 	p.readToken()
@@ -54,7 +69,7 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseReturnStatement()
 	}
 
-	return nil
+	return p.parseExpressionStatement()
 }
 
 func (p *Parser) parseLetStatement() ast.Statement {
@@ -95,6 +110,32 @@ func (p *Parser) parseReturnStatement() ast.Statement {
 	return &stmt
 }
 
+func (p *Parser) parseExpressionStatement() ast.Statement {
+	stmt := ast.ExpressionStatement{
+		Token:      p.curToken,
+		Expression: p.parseExpression(LOWEST),
+	}
+
+	if p.nextToken.Type == token.SEMICOLON { // semicolon is optional
+		p.readToken()
+	}
+
+	return &stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
 func (p *Parser) isNextToken(tt token.Type) bool {
 	if p.nextToken.Type == tt {
 		p.readToken()
@@ -118,4 +159,8 @@ func (p *Parser) skipUntilSemicolon() bool {
 
 func (p *Parser) appendErrorf(text string, args ...interface{}) {
 	p.errors = append(p.errors, fmt.Sprintf(text, args...))
+}
+
+func (p *Parser) registerPrefix(tokenType token.Type, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
 }
