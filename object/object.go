c9d88e752a8c0758d295d3dce7c15e2c97d246b0
pechorka <pechor44@gmail.com>
2021-10-11 07:45:47 +0300

    integer object

--- object/object.go
+++ object/object.go
@@ -1,8 +1,28 @@
 package object
 
+import (
+	"strconv"
+)
+
 type ObjectType string
+
+const (
+	INTEGER_OBJ ObjectType = "INTEGER"
+)
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return strconv.FormatInt(i.Value, 10)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
 }
