ac3ced3efc12284dd4198faa26cef1f6fe8b5e1d
pechorka <pechor44@gmail.com>
2021-10-11 08:08:32 +0300

    evaluating integers

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,5 +6,24 @@ import (
 )
 
 func Eval(node ast.Node) object.Object {
+	switch n := node.(type) {
+	case *ast.Program:
+		return evalStatements(n.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(n.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{
+			Value: n.Value,
+		}
+	}
+
 	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
 }
