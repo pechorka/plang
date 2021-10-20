d803d155447e883d1c8cd683a658028e95d00b0f
pechorka <pechor44@gmail.com>
2021-10-20 09:21:44 +0300

    another test for macro

--- evaluator/macro_test.go
+++ evaluator/macro_test.go
@@ -169,6 +169,18 @@ let reverse = macro(a, b) { quote(unquote(b) - unquote(a)); };
 reverse(2 + 2, 10 - 5);`,
 			`(10 - 5) - (2 + 2)`,
 		},
+		{
+			`
+let unless = macro(condition, consequence, alternative) {
+    quote(if (!(unquote(condition))) {
+        unquote(consequence);
+    } else {
+        unquote(alternative);
+}); };
+unless(10 > 5, puts("not greater"), puts("greater"));
+`,
+			`if (!(10 > 5)) { puts("not greater") } else { puts("greater") }`,
+		},
 	}
 
 	for _, tt := range tests {
